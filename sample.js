193b74a2694d095d8a81a8fbf73be80e278da1e0
James <woghd8503@naver.com>
2021-08-10 00:02:56 +0900

    HTML_Basic

---
+++ sample.js
@@ -0,0 +1,29 @@
+
+// 출력하기
+/*
+    어디로 무엇을
+    콘솔탭 문자열
+*/
+// console.log('first output')
+// // 화면에 문자열을
+// document.write('secondoput')
+
+// // 입력받기
+// var value = prompt('첫번째입력', 'placeholder')
+// console.log( typeof(value));
+
+// var result = value + 10
+// console.log(result);
+
+// scope
+// var v1;
+// {
+//     v2 = 2;
+//     let v3;
+//     const v4 = 4;
+//     console.log( v1, v2, v3, v4);
+//     v3 = v1 + v2;
+//     console.log(v3);
+// }
+// console.log(v2);
+// console.log(v3);
